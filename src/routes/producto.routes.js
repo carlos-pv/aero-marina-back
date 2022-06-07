0fcd5e7ec2a1b207c7ea770e5a644347da5473c4
carlos-pv <paraqvaria.it.1@gmail.com>
2022-06-07 14:49:18 -0400

    ADD_COLUMN

--- src/routes/producto.routes.js
+++ src/routes/producto.routes.js
@@ -1,8 +1,8 @@
 const productoController = require("../controllers/producto.controller");
 
 module.exports = (app) => {
-  app.get("/productos", productoController.list);
-  app.get("/productos-filter", productoController.listFilter);
+  app.get("/producto", productoController.list);
+  app.get("/producto-filter", productoController.listFilter);
   app.get("/producto/find/:id", productoController.getById);
   app.post("/producto/create", productoController.create);
   app.put("/producto/update/:id", productoController.update);
