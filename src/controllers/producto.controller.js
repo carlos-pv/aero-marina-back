93c0883f29c6acdc37e9f83f228f5150334e4880
carlos-pv <paraqvaria.it.1@gmail.com>
2022-06-07 13:46:51 -0400

    change_to_data

--- src/controllers/producto.controller.js
+++ src/controllers/producto.controller.js
@@ -2,37 +2,37 @@ const productoService = require("../services/producto.service");
 
 const list = async (req, res) => {
 
-  const producto = await productoService.list(req.query.q);
+  const data = await productoService.list(req.query.q);
   res.send({
     success: true,
-    producto,
+    data,
   });
 };
 
 const listFilter = async (req, res) => {
-  const producto = await productoService.listFilter(req.query.q);
+  const data = await productoService.listFilter(req.query.q);
   res.send({
     success: true,
-    producto,
+    data,
   });
 };
 
 const getById = async (req, res) => {
-  const producto = await productoService.getById(req.params.id);
+  const data = await productoService.getById(req.params.id);
 
   const jsonResultado = req.query;
   jsonResultado["success"] = true;
-  jsonResultado["producto"] = producto;
+  jsonResultado["producto"] = data;
 
   res.status(200).send(jsonResultado);
 };
 
 const create = async (req, res) => {
   try {
-    const producto = await productoService.create(req.body);
+    const data = await productoService.create(req.body);
     res.status(200).send({
       success: true,
-      producto,
+      data,
     });
     
   } catch (error) {
@@ -44,11 +44,11 @@ const create = async (req, res) => {
 };
 
 const update = async (req, res) => {
-  const producto = await productoService.update(req.body, req.params.id);
-  console.log("datos actualizacion", producto);
+  const data = await productoService.update(req.body, req.params.id);
+  console.log("datos actualizacion", data);
   res.status(202).send({
     success: true,
-    producto,
+    data,
   });
 };
 
